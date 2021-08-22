fbf876a53f794504e96dd23a90dcb9c8d434eee6
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2021-08-22 01:27:43 -0500

    Inicio

---
+++ src/main/java/com/empresa/Sesion05InicioApplication.java
@@ -0,0 +1,13 @@
+package com.empresa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sesion05InicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sesion05InicioApplication.class, args);
+	}
+
+}
