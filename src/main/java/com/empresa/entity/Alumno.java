ebcae22143bf475fda77ed4dcaa071f87fdb2e22
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2021-08-22 02:32:14 -0500

    Ajustes

--- src/main/java/com/empresa/entity/Alumno.java
+++ src/main/java/com/empresa/entity/Alumno.java
@@ -2,7 +2,6 @@ package com.empresa.entity;
 
 import java.util.Date;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -21,7 +20,6 @@ public class Alumno {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "idalumno")
 	private Integer idAlumno;
 	
 	private String nombre;
